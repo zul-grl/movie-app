2a1f1b55e1dab4556b91626bb0dcc9a0efd3186a
24LP7683 24LP7683 <24LP7683@G304-CC0225.local>
2025-01-20 12:59:58 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+type Movietype = {
+  adult: boolean;
+  backdrop_path: string;
+  original_title: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export default function Home() {
+  const [movies, setMovies] = useState<Movietype[] | undefined>();
+  const token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODk2ZmM0MDAyNjgzOGYxMDgxODE3ZTkzYzNhMDM4ZSIsIm5iZiI6MTczNzM0MjI0Ny4xOTEsInN1YiI6IjY3OGRiZDI3NjhlMGQ4NzM2MzZlMWExYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wl3C8841nWh_9RLFjnKLRMH2ZXaPdU46spcQ1YCnWtk";
+  const getMovie = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (data?.results) {
+      setMovies(data.results);
+    }
+  };
+  useEffect(() => {
+    getMovie();
+    console.log("movie");
+  }, []);
+  // console.log({ movies });
+  return (
+    <div>
+      {movies.map((movie) => {
+        return <div>{movie?.original_title}</div>;
+      })}
+    </div>
+  );
+}
